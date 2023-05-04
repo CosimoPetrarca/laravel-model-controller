b713d99bc80a2c0eaeb7f7a25f235c695c3a81a7
P4anix <pancok_1@yahoo.it>
2023-05-04 17:58:40 +0200

    prova

--- resources/views/details.blade.php
+++ resources/views/details.blade.php
@@ -6,21 +6,6 @@
          @vite('resources/js/app.js')
     </head>
     <body>
-        {{-- <div class="container p-5">
-            <div class="row">
-                <h1 class="text-center pb-5">{{ $movies->title }}</h1>
-                <div class="card col-4 ">
-                    <div class="card-body">
-                        <h5 class="card-title">{{ $movies->title }}</h5>
-                        <h6 class="card-subtitle mb-2 text-body-secondary">Titolo originale: {{ $movies->original_title }}</h6>
-                        <p class="card-text">Nazionalità: {{ $movies->nationality }}</p>
-                        <p class="card-text">Data di Uscita: {{ $movies->date }}</p>
-                        <p class="card-text">Voto: {{ $movies->vote }}</p>
-                        <a href="{{ route('home', $movies) }}" class="card-link">Torna alla Home</a>
-                    </div>
-                </div>
-            </div>
-        </div> --}}
         <div class="container p-5 text-center text-white">
             <h1>{{ $movies->title }}</h1>
             <h6 class="card-subtitle mb-2 text-body-secondary">Titolo originale: {{ $movies->original_title }}</h6>
